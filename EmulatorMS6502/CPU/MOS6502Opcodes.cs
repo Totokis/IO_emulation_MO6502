890b02f1b48d940e0bbb14325259a34f3423434a
astisk <pobierac@gmail.com>
2020-12-28 20:04:24 +0100

    Added opcodes ADC, BCS and BNE

--- EmulatorMS6502/CPU/MOS6502Opcodes.cs
+++ EmulatorMS6502/CPU/MOS6502Opcodes.cs
@@ -4,8 +4,8 @@ using System.Text;
 
 // OPCODE
 namespace EmulatorMOS6502.CPU {
-    
-    public partial class MOS6502 {
+	using UInt8 = Byte;
+	public partial class MOS6502 {
         //Tu implementujemy wszystkie opcody
 
         //Totalnie przykłaowy opcode
@@ -13,5 +13,75 @@ namespace EmulatorMOS6502.CPU {
             fetch();
             return false;
         }
-    }
+
+		UInt8 ADC() //Dodawanie
+		{
+			fetch();
+			
+			//wynik powinien być w UInt8 ale ustawiamy na UInt16 żeby było prościej zaznaczyć flagi (przy dodawaniu można np. wyjść poza zakres)
+			UInt16 result = (UInt16)((UInt16)a + (UInt16)fetched + (UInt16)getFlag('C'));
+
+			// trzeba ustawić carry bit jeżeli wynik jest większy niż 255 (bo wynik i tak musi być podany w UInt8 a tutaj mamy UInt16)
+			if (result > 255)
+				setFlag('C', true);
+			else
+				setFlag('C', false);
+
+
+			//jezeli wynik jest rowny 0 to ustawiamy flagę zero na true
+			bool parameterZ = ((result & 0x00FF) == 0);
+			setFlag('Z', parameterZ);
+			
+			//poniższe działanie sprawdza czy nie nastąpił overflow i powinno zwrócić wartość true w dwóch przypadkach: 
+			// dodatnia+dodatnia==ujemna
+			// ujemna+ujemna==dodatnia
+			bool parameterV = Convert.ToBoolean((UInt16)((~((UInt16)a ^ (UInt16)fetched) & ((UInt16)a ^ (UInt16)result)) & (UInt16)0x0080));
+			setFlag('V', parameterV);
+
+			//pierwszy bit oznacza liczbę ujemną, więc jeżeli jest pierwszy bit to ustawiamy negative na true
+			if (Convert.ToBoolean(result & 0x80))
+				setFlag('N', true);
+			else
+				setFlag('N', false);
+
+			//zapisujemy wynik do akumulatora z uwzględnieniem zamiany liczby na UInt8 (był potrzebny UInt16 żeby było łatwiej)
+			a = (UInt8)(result & 0x00FF);
+
+			return 1;
+		}
+
+		// Instruction: Branch if Carry Set
+		// Function:    if(C == 1) pc = address
+		UInt8 BCS()
+		{
+			//wykonanie instrukcji jedynie jeżeli jest carry bit
+			if (getFlag('C') == 1)
+			{
+				cycles++;
+				absAddress = (UInt16)(programCounter + relAddress);
+
+				if ((absAddress & 0xFF00) != (programCounter & 0xFF00))
+					cycles++;
+
+				programCounter = absAddress;
+			}
+			return 0;
+		}
+
+		UInt8 BNE()
+		{
+			//wykonanie instrukcji jedynie jeżeli nie ma ustawionej flagi zero
+			if (getFlag('Z') == 0)
+			{
+				cycles++;
+				absAddress = (UInt16)(programCounter + relAddress);
+
+				if ((absAddress & 0xFF00) != (programCounter & 0xFF00))
+					cycles++;
+
+				programCounter = absAddress;
+			}
+			return 0;
+		}
+	}
 }
