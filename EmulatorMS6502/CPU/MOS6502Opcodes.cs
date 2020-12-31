3d1f20f4c3395480fa856f53dfbaf8a9714ac933
Norbert Janas <norbertjanas99@gmail.com>
2020-12-31 10:42:20 +0100

    Instrukcja AND

--- EmulatorMS6502/CPU/MOS6502Opcodes.cs
+++ EmulatorMS6502/CPU/MOS6502Opcodes.cs
@@ -13,5 +13,22 @@ namespace EmulatorMOS6502.CPU {
             fetch();
             return false;
         }
+
+        // Bitowe AND dla tego co pobrane z pamięci oraz Akumulatora
+        bool AND()
+        {
+            // Pobieramy dane do zmiennej fetched
+            fetch();
+            // Wykonujemy bitowe AND dla akumulatora
+            a &= fetched;
+            // Ustawiamy dwie flagi Z oraz N
+            // Jeśli wynikiem jest 0
+            setFlag('Z', a == 0x00);
+            // Jeśli 8 bit jest równy 'zapalony'
+            setFlag('N', (Byte)(a & 0x80) == 0x80);
+
+            // Instrukcja może zwrócić dodatkowy bit, jeśli granica strony jest przekroczona to dodajemy +1 cykl
+            return true;
+        }
     }
 }
