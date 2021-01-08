312861cea13afd18cf9aa1bd550cf750d6afdb0b
Paweł <pawelj_janusz@wp.pl>
2021-01-08 18:29:27 +0100

    Commit

--- EmulatorMS6502/CPU/MOS6502AdModes.cs
+++ EmulatorMS6502/CPU/MOS6502AdModes.cs
@@ -210,6 +210,7 @@ namespace EmulatorMOS6502.CPU {
         {
             //zakładamy, że będziemy używać następnego byte'a, więc ustawiamy absAddress na programCounter
             absAddress = programCounter;
+            Console.WriteLine($"--Addressing Mode{absAddress}--");
             programCounter++;
             return false;
         }
--- EmulatorMS6502/Program.cs
+++ EmulatorMS6502/Program.cs
@@ -5,7 +5,7 @@ namespace EmulatorMS6502 {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
 
-            Computer computer = new Computer(1024);
+            Computer computer = new Computer(24);
             computer.StartComputer();
         }
     }
