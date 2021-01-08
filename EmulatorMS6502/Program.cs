f5eac27c86fe71fbf7948ae2e54c401c39b3d030
Paweł <pawelj_janusz@wp.pl>
2021-01-08 17:15:05 +0100

    Very basic implementation of GUI

--- EmulatorMS6502/Program.cs
+++ EmulatorMS6502/Program.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using EmulatorMOS6502.CPU;
 
 namespace EmulatorMS6502 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            
-            Bus bus = new Bus(1024);
-            MOS6502 mos6502 = new MOS6502(bus);
-            //mos6502.startDemo();
-            foreach (var cell in bus.Ram)
-            {
-                Console.Write(cell+" ");
-            }
+
+            Computer computer = new Computer(1024);
+            computer.StartComputer();
         }
     }
 }
