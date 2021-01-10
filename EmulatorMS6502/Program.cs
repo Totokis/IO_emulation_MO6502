9da3b9480cb711333736ae95bdc9b59a674a4934
Mordtimer <filipgab99@gmail.com>
2021-01-10 23:14:53 +0100

    Added Singletons

--- EmulatorMS6502/Program.cs
+++ EmulatorMS6502/Program.cs
@@ -5,10 +5,10 @@ using EmulatorMOS6502.CPU;
 namespace EmulatorMS6502 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
 
-            Computer computer = new Computer(24);
-            computer.StartComputer();
+            Computer.Instance.initComputer(965535);
+            Computer.Instance.StartComputer();
+           
         }
     }
 }
